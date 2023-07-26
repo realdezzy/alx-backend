9d63c50a130620734d0fb0bc76fd6208d5afb1cc
realdezzy <realdezzy1@gmail.com>
2023-07-26 21:00:54 +0100

    Task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" BaseCaching module
+"""
+
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError(
+            "put must be implemented in your cache class")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError(
+            "get must be implemented in your cache class")
+
+
+class BasicCache(BaseCaching):
+    """ Basic cache class"""
+    def put(self, key, item):
+        """ Put a key/value pair into the cache"""
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get a key/value pair from the cache"""
+        try:
+            return self.cache_data[key]
+        except KeyError:
+            return None
