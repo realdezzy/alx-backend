561fd0826a48592e551f11428fb46b2e8236e817
realdezzy <realdezzy1@gmail.com>
2023-07-29 01:44:23 +0100

    Task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,19 +1,34 @@
-#!/usr/bin/env python3
-BaseCaching = __import__("baseCaching").BaseCaching
+#!/usr/bin/python3
+""" Fifo Cache"""
+from base_caching import BaseCaching
+from collections import deque
    
 
 class FIFOCache(BaseCaching):
     """
-
-    Args:
-        BaseCaching (_type_): _description_
+        Fifo Cache implementation
     """
     def __init__(self):
         """ Initialize the cache"""
         super().__init__()
+        self.queue = deque()
 
     def put(self, key, item):
-        """ Put an item"""
+        """ Add an item in the cache
+        """
+        if not key or not item:
+            return
+        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            removed_key = self.queue.popleft()
+            self.cache_data.pop(removed_key)
+            print('DISCARD: {}'.format(removed_key))
+
+        self.cache_data[key] = item
+        self.queue.append(key)
 
     def get(self, key):
-        """ Get an item by key"""
+        """ Get a key/value pair from the cache"""
+        try:
+            return self.cache_data[key]
+        except KeyError:
+            return None
